55b2e28e412779d4239352cccc2d98eb5e2396c0
David Bustamante Portal <bustamante24.1989@gmail.com>
2023-06-22 00:07:59 -0700

    localContext

---
+++ KeyWorkerService/KeyWorkerService.Domain/Models/LocalContext/Usuario.cs
@@ -0,0 +1,12 @@
+﻿namespace KeyWorkerService.Domain.Models.LocalContext;
+
+public partial class Usuario
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? LastName { get; set; }
+
+    public int? Age { get; set; }
+}
